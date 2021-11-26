8faaf73b3741a214ef83f2b4dd90af6b4bd3410b
Olaf-II <nicholas.games26@gmail.com>
2021-11-26 16:41:27 +1100

    Fixed Update bug

--- Update.py
+++ Update.py
@@ -5,6 +5,15 @@ import time
 from colorama import Fore
 from git import RemoteProgress
 
+def finished():
+    input(f"{Fore.GREEN}Update Complete \n{Fore.RED}Drag all contents from the new Update folder into your Cryptor directory.\nAfter you have done this\n{Fore.LIGHTBLACK_EX}Press Enter To Continue")
+    try:
+        os.system('python ./Start.py')
+        shutil.rmtree('./Update')
+    except:
+        print("You have not moved contents from the Update folder to your local directory.")
+        finished()
+
 try:   
     git.Repo.clone_from('https://github.com/Olaf-II/Cryptor', './Update', branch='main')
 
@@ -17,8 +26,7 @@ try:
     os.remove('Start.py')
     os.remove('Update/Update.py')
 
-    input(f"{Fore.GREEN}Update Complete \n{Fore.RED}Drag all contents from the new Update folder into your Cryptor directory.\nAfter you have done this\n{Fore.LIGHTBLACK_EX}Press Enter To Continue")
-    os.close()
+    finished()
 
 
 except:
